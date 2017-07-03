4a6e1881ac1b0b77fb4a2ff7ef4f75b4d9681d2e
Beth Cummings <bethios@gmail.com>
2017-07-03 09:30:11 -0500

    add and sort by priority

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   }
 
   componentWillMount(){
-      let todosRef = fire.database().ref('todos').orderByKey().limitToLast(100);
+      let todosRef = fire.database().ref('todos').orderByChild('priority').limitToLast(100);
       todosRef.on('child_added', snapshot=> {
           let todo = {text: snapshot.val(), id: snapshot.key};
           this.setState({todos: [todo].concat(this.state.todos) })
@@ -17,13 +17,13 @@ class App extends Component {
   }
 
   addTodo(e){
-      e.preventDefault();
       let t = Date.now();
       let d = t + (1000 * 60 * 60 * 24 * 7);
       fire.database().ref('todos').push({
           task: this.inputEl.value,
           due: d,
-          complete: false
+          complete: false,
+          priority: Number(this.priority.value)
       });
       this.inputEl.value = "";
   }
@@ -35,20 +35,28 @@ class App extends Component {
 
   render(){
       return(
-      <form onSubmit={this.addTodo.bind(this)}>
-          <h1>Add New To-Do</h1>
-          <input type="text" ref={ el => this.inputEl = el }/>
-          <input type="submit"/>
+      <div className="list">
+          <form onSubmit={this.addTodo.bind(this)}>
+              <h1 className="tasks-header">Current Tasks</h1>
+              <p className="add-task-header describe">Task Description</p>
+              <p className="add-task-header priority">Priority</p>
+              <input type="text" placeholder="Add New Task" className="add-task add-task-field" ref={ el => this.inputEl = el }/>
+              <select name="priority" className="add-task" ref={ el => this.priority = el}>
+                  <option value="3">High</option>
+                  <option value="2">Medium</option>
+                  <option value="1">Low</option>
+              </select>
+              <input className="add-task add-task-button" type="submit"/>
 
-          <h2 className="tasks-header">Current Tasks</h2>
-          <ul>
-            {
-                this.state.todos.filter(todo => this.due(todo.text.due) >= 0)
-                    .filter(todo => todo.text.complete !== true)
-                    .map(todo => <li key={todo.id} >{todo.text.task}, Time Remaining: { this.due(todo.text.due) } days </li> )
-            }
-        </ul>
-      </form>
+              <ul>
+                {
+                    this.state.todos.filter(todo => this.due(todo.text.due) >= 0)
+                        .filter(todo => todo.text.complete !== true)
+                        .map(todo => <li key={todo.id} >{todo.text.task}, Time Remaining: { this.due(todo.text.due) } days </li> )
+                }
+            </ul>
+          </form>
+      </div>
       );
   }
 }
